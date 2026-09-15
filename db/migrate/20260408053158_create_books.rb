320107ea2ae123e8a7c8f1e3ce38a8f10cb07bf0
EikiTsuchikiri <tsuchikiri.eiki.infratop@gmail.com>
2026-09-16 00:53:31 +0900

    ユーザー一覧、詳細、編集ページ作成

--- db/migrate/20260408053158_create_books.rb
+++ db/migrate/20260408053158_create_books.rb
@@ -3,6 +3,7 @@ class CreateBooks < ActiveRecord::Migration[8.0]
     create_table :books do |t|
       t.string :title, null: false
       t.string :body, null: false
+      t.references :user
       t.timestamps
     end
   end
